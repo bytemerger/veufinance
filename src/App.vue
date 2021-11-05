7cdc0be62657feed7e452bf9b3a006c913bac0d5
franc <onyishif@gmail.com>
2021-11-05 11:16:04 +0100

    add sidebar add font awesome

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mx-0">
+    <div class="flex">
+      <side-bar class="flex-grow-0"></side-bar>
+      <main class="flex-grow">
+        <Header></Header>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import SideBar from "./components/layout/SideBar";
+import Header from "./components/layout/Header";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    SideBar
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/layout/SideBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-col p-4 align-middle h-screen">
+    <div class="inline-block mb-10">
+      <img src="../../assets/logoceed.png"  alt="ceed_cap logo"/>
+    </div>
+    <div class="flex flex-col justify-between items-center h-5/6">
+      <div>
+        <div class="text-gray-400 p-2"><i class="fas fa-arrows-alt"></i></div>
+        <div class="text-gray-400 p-2"><i class="fas fa-list-ul"></i></div>
+        <div class="text-gray-400 p-2"><i class="fas fa-envelope-open-text"></i></div>
+        <div class="text-gray-400 p-2"><i class="fas fa-credit-card"></i></div>
+        <div class="text-gray-400 p-2"><i class="fas fa-wallet"></i></div>
+        <div class="text-gray-400 p-2"><i class="fas fa-chart-line"></i></div>
+        <div class="text-gray-400 p-2"><i class="fas fa-folder"></i></div>
+      </div>
+        <!--down -->
+      <div>
+        <div class="text-gray-400 p-2"><i class="fas fa-sliders-h"></i></div>
+        <div class="text-gray-400 p-2 pt-4"><i class="fas fa-door-open"></i></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar"
+}
+</script>
+
+<style scoped>
+
+</style>
