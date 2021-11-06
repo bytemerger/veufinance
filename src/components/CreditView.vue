31c7a07d5af436dbb574d21f20b6460cdf6aea2e
franc <onyishif@gmail.com>
2021-11-06 16:20:35 +0100

    add active class for credit nav

--- src/components/CreditView.vue
+++ src/components/CreditView.vue
@@ -4,10 +4,10 @@
     <div class="text-xs text-gray-500 mt-2.5">You can find all the information about your credit card here</div>
     <div class="mt-8 text-sm">
       <ul class="flex list-none">
-        <li class="border-b pr-5 lg:pr-8 pb-2 border-opacity-25">Budget</li>
-        <li class="border-b pr-5 lg:pr-8 pb-2 border-opacity-25">Spending</li>
-        <li class="border-b pr-5 lg:pr-8 pb-2 border-opacity-25">Balance</li>
-        <li class="border-b pr-5 lg:pr-8 pb-2 border-opacity-25">Value</li>
+        <li class="border-b pr-5 lg:pr-8 pb-2 border-opacity-25 active">Budget</li>
+        <li class="border-b pr-5 lg:pr-8 pb-2 border-opacity-25 text-gray-600">Spending</li>
+        <li class="border-b pr-5 lg:pr-8 pb-2 border-opacity-25 text-gray-600">Balance</li>
+        <li class="border-b pr-5 lg:pr-8 pb-2 border-opacity-25 text-gray-600">Value</li>
       </ul>
     </div>
     <div class="card-holder masked overflow-x-auto h-120">
@@ -42,5 +42,18 @@ export default {
 }
 .h-120{
   height: 26rem;
+}
+.active{
+  position: relative;
+
+}
+.active::before {
+  content: '';
+  display: block;
+  position: absolute;
+  bottom: 0;
+  width: 75%;
+  left: 0;
+  border-top: 1px solid black;
 }
 </style>
