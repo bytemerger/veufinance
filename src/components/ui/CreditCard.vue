cfb145890c80c29baa990a0bfe25a6c998543c89
franc <onyishif@gmail.com>
2021-11-07 11:19:10 +0100

    remove unnecessary styles

--- src/components/ui/CreditCard.vue
+++ src/components/ui/CreditCard.vue
@@ -1,7 +1,6 @@
 <template>
- <div>
-   <div class="rounded shadow-xl">
-     <div class="rounded-t" :class="[ light ? 'car-light' : 'card-dark']">
+   <div class="shadow-xl overflow-hidden border-l-2 border-r-2 border-t-2 border-gray-100">
+     <div :class="[ light ? 'car-light' : 'card-dark']">
        <div class="p-5">
          <div class="flex justify-between mb-5">
            <div class="text-sm"  v-bind:class="[ light ? 'text-gray-800' : 'text-gray-300']" >Helen Spanks</div>
@@ -10,7 +9,7 @@
          <div class="text-2xl font-semibold mb-4" :class="[ light ? 'text-gray-800' : 'text-gray-100']">$42,870,00</div>
        </div>
      </div>
-     <div class="flex justify-around p-3 rounded-b" :class="[ light ? 'bg-gray-50' : 'bg-blue-200']">
+     <div class="flex justify-around p-3" :class="[ light ? 'bg-gray-50' : 'bg-blue-200']">
        <div>
          <div class="small-text text-gray-400">Card number</div>
          <div class="text-xs text-gray-800">**** **** **** 1290</div>
@@ -28,7 +27,6 @@
        </div>
      </div>
    </div>
- </div>
 </template>
 
 <script>
