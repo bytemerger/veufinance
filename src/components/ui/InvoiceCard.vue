af172baa06336440b933b32d74db3a8841360777
franc <onyishif@gmail.com>
2021-11-08 22:36:49 +0100

    fix orientation for small screen

--- src/components/ui/InvoiceCard.vue
+++ src/components/ui/InvoiceCard.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col rounded-xl md:rounded border border-opacity-35 md:border-opacity-30 p-3 lg:w-1/4 md:w-1/2 md:mr-10 mt-3 md:mt-0">
     <div class="text-gray-500 md:text-gray-400 md:text-xs mb-1.5">#43256723</div>
     <div class="font-medium text-xl md:text-sm mb-1.5">App Store</div>
-    <div class="text-gray-400 very-small-text mb-1.5">12 Jun 2021, 11:00pm</div>
+    <div class="text-gray-400 text-sm very-small-text mb-1.5">12 Jun 2021, 11:00pm</div>
     <div class="border-b border-opacity-30 mb-2.5"></div>
     <div class="flex items-center">
       <div class="font-semibold md:font-medium md:text-sm">$120,00</div>
